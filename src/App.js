c3307990ecb6953e27b6f22f4f1cccada7ba4c99
amit thakur <amit.thakur@softobiz.com>
2024-01-08 12:34:21 +0530

    fronentend setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import Chat from './components/Chat';
+import Nav from './components/Nav';
+import { usePubNub } from 'pubnub-react';
 function App() {
+  const pubnub = usePubNub();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Nav/>
+      </div>
+      <Routes>
+        <Route path="/" element={<Chat />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+
   );
 }
 
